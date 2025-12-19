aa7351058853ddf607fe41ef9ecc1d8d6db29d64
louayAl <louae.2002@gmail.com>
2025-12-19 12:50:48 +0100

    Backend: initialize Spring Boot project (Java 21)

---
+++ backend/formation/src/main/java/com/s3m/formation/S3mFormationApplication.java
@@ -0,0 +1,13 @@
+package com.s3m.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3mFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3mFormationApplication.class, args);
+	}
+
+}
